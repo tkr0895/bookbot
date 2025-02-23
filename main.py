c22f886f9dd80d6d4ab21de59458bd0fc1a76f6e
tkr0895 <TomKramer95@gmail.com>
2025-02-23 22:09:04 +0100

    use split_dictonary in main

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from stats import count_words
+from stats import count_characters
+from stats import split_dictonary
+
 def get_book_text(book):
     with open(book) as f:
         return f.read()
-    
-from stats import count_words
-from stats import count_characters
 
 def main():
     word_count = count_words(get_book_text("./books/frankenstein.txt"))
     char_count = count_characters(get_book_text("./books/frankenstein.txt"))
+    char_list = split_dictonary(char_count)
     print(f"{word_count} words found in the document")
-    print(char_count)
+    print(char_list)
 
 main()
