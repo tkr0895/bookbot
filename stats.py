84713e0649fc76b33db58beee96e3cb666951849
tkr0895 <TomKramer95@gmail.com>
2025-02-23 21:38:39 +0100

    add count_characters function to stats

--- stats.py
+++ stats.py
@@ -4,3 +4,15 @@ def count_words(booktext):
     for word in words:
         word_count += 1
     return word_count
+
+def count_characters(booktext):
+    characters = {}
+    words = booktext.split()
+    for word in words:
+        for c in word:
+            c = c.lower()
+            if c in characters:
+                characters[c] += 1
+            else:
+                characters[c] = 1
+    return characters
