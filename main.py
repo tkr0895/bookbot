6c70ac8d1afe1b84a57e06c37675a17834410c8f
tkr0895 <TomKramer95@gmail.com>
2025-02-23 21:38:53 +0100

    use count_characters in main

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ def get_book_text(book):
         return f.read()
     
 from stats import count_words
+from stats import count_characters
 
 def main():
     word_count = count_words(get_book_text("./books/frankenstein.txt"))
+    char_count = count_characters(get_book_text("./books/frankenstein.txt"))
     print(f"{word_count} words found in the document")
+    print(char_count)
 
 main()
