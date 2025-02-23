bef498604df71527c5fb40c143ad5c2dbf7da014
tkr0895 <TomKramer95@gmail.com>
2025-02-23 22:17:08 +0100

    add print_report to main

--- main.py
+++ main.py
@@ -5,12 +5,23 @@ from stats import split_dictonary
 def get_book_text(book):
     with open(book) as f:
         return f.read()
+    
+def print_report(path, total_words, character_count):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print(f"--------- Character Count -------")
+    for dict in character_count:
+        if dict["char"].isalpha():
+            print(f"{dict["char"]}: {dict["num"]}")
+    print("============= END ===============")
 
 def main():
-    word_count = count_words(get_book_text("./books/frankenstein.txt"))
-    char_count = count_characters(get_book_text("./books/frankenstein.txt"))
+    path = "./books/frankenstein.txt"
+    word_count = count_words(get_book_text(path))
+    char_count = count_characters(get_book_text(path))
     char_list = split_dictonary(char_count)
-    print(f"{word_count} words found in the document")
-    print(char_list)
+    print_report(path, word_count, char_list)
 
 main()
